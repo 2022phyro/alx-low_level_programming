8a97155811038cb1a977b186508db1aa254f6a7a
2022phyro <phyrokelstein2@gmail.com>
2022-09-08 08:11:37 -0700

    no 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*print out a sentence using the puts function*/
+int main(void)
+{
+	char name[50] = Programming is like building a multilingual puzzle;
+
+	puts(name);
+	return (0);
+}
