baf5e2908d810bb26675d07ec508640649440494
2022phyro <phyrokelstein2@gmail.com>
2022-09-08 12:49:50 -0700

    no 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
  * print out a sentence using the puts function
- * main:use puts function to display program result
- * return:return zero if executed properly, else a non-zero value
- */
+ * main - use puts function to display program result
+ * return: return zero if executed properly, else a non-zero value
+ */ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
