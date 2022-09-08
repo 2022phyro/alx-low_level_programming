b371b2c37d24de561354691cf4accd15e16415ee
2022phyro <phyrokelstein2@gmail.com>
2022-09-08 12:58:25 -0700

    no 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,12 +4,12 @@
  * main - uses printf and aizeof to give our result
  * Return:0 if executed properly else a non zero value
  */
- int main(void)
+int main(void)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d byte(s)\n", sizeof(int))
-printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a double: %d byte(s)\n", sizeof(double));
+printf("Size of a char: %d byte(s) \n", sizeof(char));
+printf("Size of an int: %d byte(s) \n", sizeof(int))
+printf("Size of a long int: %d byte(s) \n", sizeof(long int));
+printf("Size of a long long int: %d byte(s) \n", sizeof(long long int));
+printf("Size of a double: %d byte(s) \n", sizeof(double));
 return (0);
 }
