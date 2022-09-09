11cca9159276325a405537171dc1231b3e65e31a
2022phyro <phyrokelstein2@gmail.com>
2022-09-09 07:29:15 -0700

    no 7 and 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 /**
- * main - it ia where the code is written
- * Description:'It prints out all single digit numbers'
- * Return: 0 if executed properly else a non zero value
+ *main - it ia where the code is written
+ *Description:'It prints out all single digit numbers'
+ *Return: 0 if executed properly else a non zero value
  */
 int main(void)
 {
-        int num;
+	int num;
 
-        num = 0;
-
-        for (num = 0; num < 10; num++)
-        putchar(num);
-	putchar(int num);
-        return (num);
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
 }
