ed4b4e64d814862838a332881afe08cf2cd0ae91
2022phyro <phyrokelstein2@gmail.com>
2022-09-09 11:54:29 -0700

    no 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this is where our code will be written
+ * Description:'Print all possible combinations of singke digits'
+ * Return: 0 if successful else a nin zero value
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58;)
+	{
+		putchar(ch);
+		putchar(',');
+		putchar(' ');
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
