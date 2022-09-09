c835105b478206381f6bbff48b695a03936dc2be
2022phyro <phyrokelstein2@gmail.com>
2022-09-08 22:56:39 -0700

    no 8 again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
-#include<unistd.h> 
+#include<unistd.h>
  /**
   *main - prints exactly a phrase about art
   *Description:'Prints a phrase withot the use of printf  or puts'
-  Return:Return 0 if the program works well else another non-zero file
-  */j
- int main(void)
+  *Return: Return 0 if the program works well else another non-zero file
+  */
+int main(void)
 {
-	write(2, "and that piece of art is useful\" - );
-	write(Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	write(2,
+"and that piece of art is useful - Dora Korpar, 2015-10-19\n",  59);
+	return (1);
 }
