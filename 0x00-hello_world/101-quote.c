4f464966d6bf4073e6ca56cade54bb7be9b8d0f6
2022phyro <phyrokelstein2@gmail.com>
2022-09-08 23:09:31 -0700

    no 8 correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
-#include<unistd.h>
- /**
-  *main - prints exactly a phrase about art
-  *Description:'Prints a phrase withot the use of printf  or puts'
-  *Return: Return 0 if the program works well else another non-zero file
-  */
-int main(void)
+#include <unistd.h>
+
+/**
+*main - makes use of a new header file
+*Description:'Prints a phrase without using printf or puts'
+*Return: 1 if successfully executed else another value
+*/
+int main(void)
 {
-	write(2,
-"and that piece of art is useful - Dora Korpar, 2015-10-19\n",  59);
-	return (1);
+write(2, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
