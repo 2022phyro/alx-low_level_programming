582bf4bbf723a4d4950e50d85f225ebf9c62ca7e
2022phyro <phyrokelstein2@gmail.com>
2022-09-11 14:28:57 -0700

    corrections

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,9 +19,10 @@ int main(void)
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
 			if (!(i == 98 && j == 99))
-
-			putchar(',');
-			putchar(' ');
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
