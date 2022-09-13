ebfcf56aa6e4345199424547fdabf54579593946
2022phyro <phyrokelstein2@gmail.com>
2022-09-13 07:18:17 -0700

    no 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+/**
+ *_islower - checks if the character is in lower case
+ *Descruptio:'a function'
+ *Return: 1 if accurate
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		_putchar('1')
+	}
+	else
+	{
+		_putchar('0')
+	}
+return (c);
+}
