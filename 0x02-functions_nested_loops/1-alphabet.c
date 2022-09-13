7c77c87f838c454866499e7af4e55d796141c78a
2022phyro <phyrokelstein2@gmail.com>
2022-09-13 05:53:11 -0700

    no 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints all the leyters of the alphabet
+ * Description: uses a loop
+ * Return: 0 if success
+ */
+void print_alphabet(void)
+	{int i;
+		for ( i = a; i <= z; i++)
+		{
+		_putchar(i);
+		}
+		_putchar(\n);
+	
+	}
+
