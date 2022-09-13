707918b39233be5e9cce35f423f03da297717cbd
2022phyro <phyrokelstein2@gmail.com>
2022-09-13 06:51:01 -0700

    no 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints all the letters of the alphabet 10 times
+ * Descruptio:'a function'
+ * Return: 0 if accurate
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	
+	}
+}
