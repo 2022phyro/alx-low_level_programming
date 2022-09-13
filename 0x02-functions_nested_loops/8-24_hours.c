8fa7a0d8aac0aca8b9c8d4ecb41c0c2ad977188f
2022phyro <phyrokelstein2@gmail.com>
2022-09-13 11:03:14 -0700

    no 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - prints out all the minutes of the day
+ * Return: 0 if successful
+ * @void: data type of the values being printed out
+ */
+void jack_bauer(void)
+{
+	int t, u;
+
+		for (t = 0; t < 24; t++)
+		{
+			for (u = 0; u < 60; u++)
+			{
+				_putchar(t / 10 + '0');
+				_putchar(t % 10 + '0');
+				_putchar('.');
+				_putchar(u / 10 + '0');
+				_putchar(u % 10 + '0');
+			}
+			if (!(t == 23 && u == 59))
+				_putchar('\n');
+		}
+}
