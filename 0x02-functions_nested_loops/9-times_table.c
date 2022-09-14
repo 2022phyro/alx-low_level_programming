b868f9dfab1920309107979e33923445b8ce4412
2022phyro <phyrokelstein2@gmail.com>
2022-09-14 04:52:05 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,8 +12,9 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 			o = m * n;
-			if(! (o <= 9))
-				_putchar(o / 10 +'0');
+			if(o <= 9)
+				_putchar(' ');
+			_putchar(o / 10 +'0');
 			_putchar(o % 10 + '0');
 		}
 		putchar ('\n');
