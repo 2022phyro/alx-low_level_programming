df679da99f7966d836fdd72e52d36607b644df19
2022phyro <phyrokelstein2@gmail.com>
2022-09-14 07:55:03 -0700

    no 11

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_tables prints the time table of an integer
+ * Description:'does the above in a new function'
+ * Return: nothing
+ * @n: the integer to be printed
+ */
+void print_times_tables(int n)
+{
+	if (!(n > 15 || n < 0))
+	{
+		int num, mult, prod;
+
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				prod = num * mult;
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar((prod / 10) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
