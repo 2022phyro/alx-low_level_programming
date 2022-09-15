b15654c8bd5dcbe7317202c340d46e8fc274269f
2022phyro <phyrokelstein2@gmail.com>
2022-09-15 13:33:34 -0700

    no 0

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	/*while (i < 10)*/
-	/*{*/
-	/*	putchar();*/
-	/*}*/
-
+/*
+*	while (i < 10)
+*	{
+*		putchar();
+*	}
+*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
