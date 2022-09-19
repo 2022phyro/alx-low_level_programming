216b28192bc8c889cd6a9999975249d268fbb9e3
2022phyro <phyrokelstein2@gmail.com>
2022-09-19 05:54:28 -0700

    no0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,12 +4,9 @@
  * reset_to_98 - it changes the number in the variable to 98
  * Descritpion:'A function'
  * Return: Nothing
- * @*n: the variable to be changed
+ * @n: the variable to be changed
  */
 void reset_to_98(int *n)
 {
-	int *p;
-	p = &n;
-	*p = 98;
-	return (p);
+	*n = 98;
 }
