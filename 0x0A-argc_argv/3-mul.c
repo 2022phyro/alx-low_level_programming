7edc77d8c9c70c360d29867c1041795641524bf4
2022phyro <phyrokelstein2@gmail.com>
2022-09-30 11:33:45 -0700

    Multiply two arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
-int main(int argc, char _attribute_ ((unused)) *argv[])
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - multiplies two arguments
+ *
+ * @argc: the number of arguments to the function
+ * @argv: the arguments to the functio
+ * Return: nothing
+ */
+int main(int argc, char *argv[])
 {
-	int i = argc + 1;
-	if (i == 2)
-	printf("Error");
-	return(argc);
+	int i, j, k;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
+	i = 0;
+	j = atoi(argv[1]);
+	k = atoi(argv[2]);
+	i = k * j;
+	printf("%d\n", i);
+	return (0);
 }
-	
--- 0x0A-argc_argv/ma.c
+++ 0x0A-argc_argv/ma.c
@@ -10,27 +10,18 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, k, l;
+	int i, j, k;
 
-	l = argc;
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+
 	i = 0;
 	j = atoi(argv[1]);
 	k = atoi(argv[2]);
-
-	if (l + 1 == 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	if (l == 3)
-	{
-		i = k * j;
-		printf("%d\n", i);
-		return (0);
-	}
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
+	i = k * j;
+	printf("%d\n", i);
+	return (0);
 }
