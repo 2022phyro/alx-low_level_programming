718efaffc4314849827e06b150c99bf745223e0f
2022phyro <phyrokelstein2@gmail.com>
2022-10-06 05:00:37 -0700

    Allocate memory

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checekd - allocates memory and points to it
+ *
+ * Return: A pointer to the allocated memory
+ * @b: the size of the memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+	free(ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+THis readme contains more on the work concerning malloc realloc. We will see how thses can be applied and how to use them.
