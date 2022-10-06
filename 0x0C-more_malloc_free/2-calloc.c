6feaaf9ac5242fa03217eb604f8ced7af4ed5b77
2022phyro <phyrokelstein2@gmail.com>
2022-10-06 13:03:45 -0700

    Mimick calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *Memset - seta a memory address
+ *
+ * @s: the memory to be set
+ * @c: the value
+ * len: the limit
+ * Return: a pointer to the set memory
+ */
+void *Memset(void *s, int c, unsigned int len)
+{
+	unsigned char *p = s;
+
+	while (len--)
+	{
+		*p++ = (unsigned char)c;
+	}
+	return (s);
+}
+/**
+ * _calloc - micks the calloc function
+ *
+ * @nmemb: the array size
+ * @size: the memory size
+ * Return: a pointer to the allocated space
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *st;
+	void *end;
+
+	st = malloc(size * nmemb);
+
+	end = Memset(st, 0, (nmemb * size));
+	return (end);
+}
