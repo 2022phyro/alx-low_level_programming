876f6fa5a303b02bf07ad49fbb049a83c0772de3
2022phyro <phyrokelstein2@gmail.com>
2022-10-10 06:27:51 -0700

    Create a new type called struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This repository contains files on practice work on Structures and the typedef keyword. It contains the answers to the alx project by the name of this directory.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOH_H
+#define DOH_H
+/**
+ * struct dog - defines a new type of struct called dog
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog;
+#endif
