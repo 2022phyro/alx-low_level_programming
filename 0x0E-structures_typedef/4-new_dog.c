bc6e310363a4a9379172131a36312070f4c8c733
2022phyro <phyrokelstein2@gmail.com>
2022-10-10 11:04:45 -0700

    Add the necessary colons to file number 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,14 +9,15 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *ptr
+	dog_t *ptr;
 
-	ptr = malloc(sizeof(dog_t))
+	ptr = malloc(sizeof(dog_t));
 
 	if (ptr == NULL)
 		exit(98);
 	ptr->name = name;
 	ptr->age = age;
 	ptr->owner = owner;
+
 	return (ptr);
 }
