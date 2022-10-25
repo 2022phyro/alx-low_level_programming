92aadd81ad1251b6763a4a8c24a6053d9b333346
2022phyro <phyrokelstein2@gmail.com>
2022-10-25 01:47:32 -0700

    Delete a node at the head

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,6 +3,7 @@
 /**
  * pop_listint - deletes the head node
  *
+ * Description:'A function'
  * @head: the head node
  * Return: the head node's data
  */
@@ -10,6 +11,9 @@ int pop_listint(listint_t **head)
 {
 	int n;
 	listint_t *temp;
+
+	if ((*head) == NULL)
+		return (0);
 
 	temp = (*head);
 	n = (*head)->n;
