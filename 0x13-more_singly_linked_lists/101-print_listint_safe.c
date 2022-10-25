1062be55b129faddb1b0921e9590ddb9efcd6fcf
2022phyro <phyrokelstein2@gmail.com>
2022-10-25 07:24:15 -0700

    Print a linked list even if there's a loop

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,9 +1,90 @@
 #include <stdio.h>
 #include "lists.h"
+/**
+ * *detect - detects the presence of a linked list
+ *
+ * @h: the head node
+ * Return: a node where the tortoise meets the hare
+ */
+const listint_t *detect(const listint_t *h)
+{
+	const listint_t *S, *F;
+
+	S = h;
+	F = h;
+
+	while (S && F && F->next)
+	{
+		S = S->next;
+		F = F->next->next;
+		if (F == S)
+		return (F);
+	}
+	return (NULL);
+}
+/**
+ * *find_listint - finds the loop
+ *
+ * @head: the head node
+ * Return: the start of the loop;
+ */
+const listint_t *find_listint(const listint_t *head)
+{
+	const listint_t *mark;
+
+	mark = detect(head);
+
+	if (mark == NULL)
+	return (NULL);
+	while (head != mark)
+	{
+		head = head->next;
+		mark = mark->next;
+	}
+	return (head);
+}
+/**
+ * print_listint_safe - prints alist
+ *
+ * @head: the head of the list
+ * Return: the nuber of nodes printed;
+ */
 size_t print_listint_safe(const listint_t *head)
 {
+	const listint_t *mark;
+	size_t flag, n;
+
 	if (head == NULL)
-		return (0);
-	printf("[%x] %d", &(head->n), head->n);
-	return (1 + (print_listint_safe(head->next)));
+		exit(98);
+	flag = 0;
+	n = 0;
+	mark = find_listint(head);
+	if (mark == NULL)
+	{
+		while (head != NULL)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+			n++;
+		}
+		return (n);
+	}
+
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		n++;
+		if (head == mark)
+		{
+			flag++;
+		}
+		if (flag == 2)
+		{
+			break;
+		}
+	}
+	printf("-> [%p] %d", (void *)mark, mark->n);
+	return (n);
 }
+
