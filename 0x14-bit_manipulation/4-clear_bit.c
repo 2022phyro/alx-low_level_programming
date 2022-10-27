87e09df1bc91c1d6b15a81072310d83c32af19aa
2022phyro <phyrokelstein2@gmail.com>
2022-10-27 15:25:38 -0700

    Fix betty errors

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,35 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * get_bit - gets a particular bit
- *
- * @index: the index
- * @n: the number
- * Return: the pointed bit
- */
-int get_bit(unsigned long int n, unsigned int index)
-{
-	unsigned int i;
-
-	i = 0;
-
-	if (index > 63)
-	{
-		;
-	}
-	else
-	{
-		while (n)
-		{
-			if (i == index)
-				return ((n & 1));
-			n = n >> 1;
-			i++;
-		}
-	}
-	return (-1);
-}
-/**
  * clear_bit -  Assign the bit to zero
  *
  * @index: the index
@@ -38,16 +9,9 @@ int get_bit(unsigned long int n, unsigned int index)
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int k;
-
-	k = (*n);
-	if (get_bit(k, index) == 1)
-	{
-		(*n) = k ^ (1 << index);
-	}
-	else if (get_bit(k, index) == -1)
-	{
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	}
+
+	(*n) = (*n) & ~(1 << index);
 	return (1);
 }
