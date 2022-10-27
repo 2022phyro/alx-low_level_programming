56f1bdb117a8e16a7ae02e293854a1bac9b22126
2022phyro <phyrokelstein2@gmail.com>
2022-10-27 15:43:29 -0700

    Fix the indentation error

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,21 +10,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i;
-
 	if (index >= sizeof((unsigned long int) * 8))
-	{
 		return(-1);
-	}
-	i = 0;
-	while (n)
-	{
-		if (i == index)
-			return ((n & 1));
-		if (n == 0)
-			break;
-		i++;
-		n = n >> 1;
-	}
-	return (-1);
+	return (n & (1 << index));
 }
