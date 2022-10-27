70d6a520b61b8a096b5375d4749043af4b702d78
2022phyro <phyrokelstein2@gmail.com>
2022-10-27 14:29:23 -0700

    Fix set mark for index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * bit_len - gives the number of bits
- *
- * @n: the integer to be checked
- * Return: the number of bits in the byte
- */
-unsigned int bit_len(unsigned long int n)
-{
-	unsigned int i;
-
-	if (n == 0)
-		return (1);
-	i = 0;
-
-	while (n)
-	{
-		i++;
-		n = n >> 1;
-	}
-	return (i);
-}
-/**
  * set_bit - seta a bit at a given mark to 1
  *
  * @n: the number
@@ -32,15 +11,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int k;
 
-	if ((*n) == 0)
-		(*n) = 0 | (1 << index);
-
-	else
-	{
-		k = (*n);
-		if (bit_len(k) - 1 < index)
+	if (index > 63)
 		return (-1);
-		(*n) = k | (1 << index);
-	}
+	k = (*n);
+	(*n) = k | (1 << index);
 	return (1);
 }
