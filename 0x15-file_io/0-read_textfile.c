8d8ac1f08f352499f6b1c0e820182d41e89c134e
2022phyro <phyrokelstein2@gmail.com>
2022-10-31 08:27:03 -0700

    Append to the end of file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ * read_textfile - reads from a file
+ *
+ * @filename: the name of the file
+ * @letters: the size of the bytes to be read
+ * Return: the number of bytes or -1 if failed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buffer;
+	ssize_t n, j;
+	size_t i;
+
+	buffer = malloc(sizeof(char) * letters)
+	i = 0;
+	if (buffer == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
+	n = open(filename, O_RDONLY, 600);
+	if (n == -1)
+		return (0);
+	j = read(n, buffer, letters);
+	if (j == -1)
+		return (0);
+	close(n);
+
+	while (buffer[i] != '\0')
+	{
+		_putchar(buffer[i]);
+		i++;
+	}
+	free(buffer);
+	return (i);
+
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,5 @@
+# C- FILE INPUT AND OUTPUT
+## Synopsis
+This directory contains knowledge and practice work on the topic File Input and Output Here, we see how to create, write and read into a file from a program or outside the file
+## Helper Files :raised hands:
+[putchcar](./_putchar.c): Serves to print out our information to standard output
