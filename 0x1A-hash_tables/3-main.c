d88d84feb60a2153716b7222238aeb6081fe2d02
2022phyro <phyrokelstein2@gmail.com>
2023-01-07 00:00:25 +0100

    Php here i come

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -7,34 +7,35 @@
  */
 int main(void)
 {
-    hash_table_t *ht;
+    shash_table_t *ht;
     char *value;
 
-    ht = hash_table_create(1024);
-    hash_table_set(ht, "c", "fun");
-    hash_table_set(ht, "python", "awesome");
-    hash_table_set(ht, "Bob", "and Kris love asm");
-    hash_table_set(ht, "N", "queens");
-    hash_table_set(ht, "Asterix", "Obelix");
-    hash_table_set(ht, "Betty", "Cool");
-    hash_table_set(ht, "98", "Battery Street");
-    hash_table_set(ht, "c", "isfun");
+    ht = shash_table_create(1024);
+    shash_table_set(ht, "c", "fun");
+    shash_table_set(ht, "python", "awesome");
+    shash_table_set(ht, "mob", "and Kris love asm");
+   shash_table_set(ht, "N", "queens");
+    shash_table_set(ht, "Asterix", "Obelix");
+    shash_table_set(ht, "Betty", "Cool");
+    shash_table_set(ht, "98", "Battery Street");
+    shash_table_set(ht, "c", "isfun");
 
-    value = hash_table_get(ht, "python");
+    value = shash_table_get(ht, "python");
     printf("%s:%s\n", "python", value);
-    value = hash_table_get(ht, "Bob");
+    value = shash_table_get(ht, "Bob");
     printf("%s:%s\n", "Bob", value);
-    value = hash_table_get(ht, "N");
+    value = shash_table_get(ht, "N");
     printf("%s:%s\n", "N", value);
-    value = hash_table_get(ht, "Asterix");
+    value = shash_table_get(ht, "Asterix");
     printf("%s:%s\n", "Asterix", value);
-    value = hash_table_get(ht, "Betty");
+    value = shash_table_get(ht, "Betty");
     printf("%s:%s\n", "Betty", value);
-    value = hash_table_get(ht, "98");
+    value = shash_table_get(ht, "98");
     printf("%s:%s\n", "98", value);
-    value = hash_table_get(ht, "c");
+    value = shash_table_get(ht, "c");
     printf("%s:%s\n", "c", value);
-    value = hash_table_get(ht, "javascript");
-    printf("%s:%s\n", "javascript", value);
+    shash_table_print_rev(ht);/*
+    value = shash_table_get(ht, "javascript");
+    printf("%s:%s\n", "javascript", value);*/
     return (EXIT_SUCCESS);
 }
