d5990be670462b711667c65eb5f1f062424d27a2
2022phyro <phyrokelstein2@gmail.com>
2023-04-13 07:44:46 -0700

    Corrected betty

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -6,38 +6,46 @@
  * @list: the list to be sorted
  * @size: the array lenght
  * @value: the value to be checked
+ * Return: the node or NULL
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
 	size_t bloc, i;
-	listint_t *temp, *mark;
+	listint_t *temp = list, *mark = list;
+
 	if (list == NULL || size == 0)
 		return (NULL);
 	bloc = sqrt(size);
 	i = bloc;
-	temp = list;
-	mark = list;
 	while (true)
 	{
+		printf("Value checked array[%lu] ", i);
 		while (temp->index != i)
 			temp = temp->next;
-		printf("Value checked array[%d] ", i);
 		printf("= [%d]\n", temp->n);
-		if (temp->n >= value || (i + bloc >= size))
+		if (temp->n >= value)
 		{
-			printf("Value found between indexes [%d] ", mark->index);
-			printf("and [%d]\n", temp->index);
+			printf("Value found between indexes [%lu] ", mark->index);
+			printf("and [%lu]\n", temp->index);
+			temp = mark;
+			break;
+		}
+		if (i + bloc >= size)
+		{
+			printf("Value found between indexes [%lu] ", temp->index);
+			printf("and [%lu]\n", size - 1);
 			break;
 		}
 		i += bloc;
 		mark = temp;
 	}
-	temp = mark;
-	while (temp != NULL) 
+	while (temp != NULL)
 	{
-		printf("Value checked array[%d] = [%d]\n", temp->index, temp->n);
+		printf("Value checked array[%lu] = [%d]\n", temp->index, temp->n);
 		if (temp->n == value)
 			return (temp);
+		if (temp->index == i)
+			return (NULL);
 		temp = temp->next;
 	}
 	return (NULL);
