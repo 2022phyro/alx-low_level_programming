f9291e911fe2efe251f901c3a1d3e2c8fedcd790
2022phyro <phyrokelstein2@gmail.com>
2023-04-13 12:32:34 -0700

    corrected 107-O

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -16,11 +16,14 @@ int jump_search(int *array, size_t size, int value)
 		return (-1);
 	block = sqrt(size);
 	i = 0;
-	while (true)
+	if (array[i] > value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		/*if (array[i] > value)
-			return (-1);*/
+		return (-1);
+	}
+	while (array[i] < value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[(int)(i + block)] >= value ||
 				i + block >= size)
 		{
